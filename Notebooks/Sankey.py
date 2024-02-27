fca4fce41ee11fce43350f95eb47dad5a61cf437
serappold <rappold.scott@gmail.com>
2024-02-27 09:11:55 -0600

    updated sankey file based on nodes and links csv

--- Notebooks/Sankey.py
+++ Notebooks/Sankey.py
@@ -10,34 +10,61 @@ import plotly.offline as py
 from plotly.offline import plot
 import plotly.graph_objects as go
 import numpy as np
+import os
 
-nodes = pd.read_csv('Data/Processed/nodes.csv')
-links = pd.read_csv('Data/Processed/links.csv')
+def make_sankey():
 
-#print(nodes.head())
-#print(links.head())
+	nodes = pd.read_csv(os.getcwd() + '/Data/03 Processed/nodes.csv')
+	links = pd.read_csv(os.getcwd() + '/Data/03 Processed/links.csv')
+	#nodes = pd.read_csv('Data/Processed/nodes.csv')
+	#links = pd.read_csv('Data/Processed/links.csv')
 
-temp = pd.DataFrame(links[links.Group_Count>225])
+	print(os.getcwd())
 
-data = dict(type='sankey',
-	node = dict(
-		pad=1,
-		thickness=5,
-		line=dict(
-			color='black',
-			width=.5),
-		label=nodes['Systems']),
-	link = dict(
-		source = temp['FromSystemID'],
-		target = temp['SystemID'],
-		value = temp['Group_Count'])
-)
+	temp = pd.DataFrame(links[(links['FromSystemID'] == 59) | (links['SystemID'] == 59)])
 
-layout = dict(
-	title = 'System to System Element Flow',
-	font = dict(
-		size = 20))
-fig = dict(data=[data], layout=layout)
+	data = dict(type='sankey',
+		node = dict(
+			pad=1,
+			thickness=5,
+			line=dict(
+				color='black',
+				width=.5),
+			label=nodes['Systems']),
+		link = dict(
+			source = temp['FromSystemID'],
+			target = temp['SystemID'],
+			value = temp['Group_Count'])
+	)
 
-print(links[links.Group_Count>225])
-py.plot(fig, validate=True)
+	data = dict(type='sankey',
+		node = dict(
+			pad=1,
+			thickness=5,
+			line=dict(
+				color='black',
+				width=.5),
+			label=nodes['Systems']),
+		link = dict(
+			source = links['FromSystemID'],
+			target = links['SystemID'],
+			value = links['Group_Count'])
+	)
+
+
+	layout = dict(
+		title = 'System to System Element Flow',
+		font = dict(
+			size = 20))
+	fig = dict(data=[data], layout=layout)
+
+	#print(links[links.Group_Count>225])
+	
+	try:
+		py.plot(fig, validate=True, filename=os.path.join(os.getcwd(), 'Reports', 'Systems.html'))
+		print('HTML File Generated')
+	except Exception as e:
+		print('Error: ', e)
+
+
+	print('completed')
